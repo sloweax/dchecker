6763107fa2db13f144a068a93f41bcd54fd0e5f1
sloweax <sloweax@users.noreply.github.com>
2023-08-08 22:31:38 -0300

    added kr/jp/it/com servers

--- src/whoisservers.cr
+++ src/whoisservers.cr
@@ -1,6 +1,7 @@
 module DChecker
   SERVERS = {
     "whois.verisign-grs.com" => {["com", "net"], /^No match for/m},
+    "whois.crsnic.net" => {["com"], /^No match for/m},
     "whois.opensrs.net" => {["com", "net"], /^Can't get information on non-local domain/m},
     "ccwhois.verisign-grs.com" => {["cc"], /^No match for/m},
     "whois.cnnic.cn" => {["cn"], /^No matching record/m},
@@ -10,5 +11,8 @@ module DChecker
     "whois.audns.net.au" => {["au"], /^NOT FOUND/m},
     "whois.dns.be" => {["be"], /Status:	AVAILABLE/m},
     "whois.nic.br" => {["br", "com.br"], /^% No match for/m},
+    "whois.kr" => {["kr"], /^The requested domain was not found/m},
+    "whois.jprs.jp" => {["jp"], /^No match!!/m},
+    "whois.nic.it" => {["it"], /^Status: +AVAILABLE/m},
   }
 end
