f0f4227b256172f5b697dae6c25d971994eebd39
sloweax <sloweax@users.noreply.github.com>
2023-08-09 00:08:37 -0300

    added query format in src/whoisservers.cr / cli parsing

--- src/dchecker.cr
+++ src/dchecker.cr
@@ -5,7 +5,7 @@ require "option_parser"
 require "colorize"
 
 module DChecker
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 
   timeout = 5.0
   fin = STDIN
@@ -15,6 +15,7 @@ module DChecker
   dynamic_server = Array(String).new
   dynamic_available_regex = Array(Regex).new
   dynamic_tlds = Array(Array(String)).new
+  dynamic_query = Array(String).new
 
   OptionParser.parse do |parser|
     parser.banner = "usage: #{PROGRAM_NAME} [options]"
@@ -40,7 +41,10 @@ module DChecker
 
     parser.on("-i FILE", "--input FILE", "reads domains from FILE") { |f| fin = File.open(f) }
 
-    parser.on("-S server", "add WHOIS server (needs -T and -R)") { |s| dynamic_server << s }
+    parser.on("-S server", "add WHOIS server (needs -T and -R. -Q is optional)") { |s|
+      dynamic_server << s
+      dynamic_query << "%domain%\n"
+    }
 
     parser.on("-T tld", "add tld to WHOIS server") { |t|
       dynamic_tlds << Array(String).new if dynamic_server.size - 1 == dynamic_tlds.size
@@ -48,6 +52,8 @@ module DChecker
     }
 
     parser.on("-R regex", "if the WHOIS server response matches with regex, mark it as an available domain") { |r| dynamic_available_regex << Regex.new(r, Regex::Options::MULTILINE) }
+
+    parser.on("-Q query", "changes WHOIS query format (default: \"%domain%\\n\")") { |q| dynamic_query[dynamic_server.size - 1] = q }
 
     parser.invalid_option do |flag|
       STDERR.puts "#{flag} is not a valid option."
@@ -57,8 +63,8 @@ module DChecker
   end
 
   begin
-    dynamic_server.zip(dynamic_tlds, dynamic_available_regex).each do |h, t, r|
-      SERVERS[h] = {t, r}
+    dynamic_server.zip(dynamic_tlds, dynamic_available_regex, dynamic_query).each do |h, t, r, q|
+      SERVERS[h] = {t, r, q}
     end
   rescue
     STDERR.puts "could not add WHOIS server"
@@ -72,13 +78,13 @@ module DChecker
   tld2client = Hash(String, NamedTuple(clients: Array(WHOISClient), channel: Channel(Domain))).new
 
   SERVERS.each do |(server, data)|
-    tlds, regex = data
+    tlds, regex, query = data
 
     tlds.each do |tld|
       tld2client[tld] = {clients: Array(WHOISClient).new, channel: Channel(Domain).new} unless tld2client.has_key? tld
     end
 
-    WHOISClient.new(server, available_regex: regex).tap do |c|
+    WHOISClient.new(server, available_regex: regex, query_format: query).tap do |c|
       tlds.each do |tld|
         tld2client[tld][:clients] << c
         c.add_input_channel(tld2client[tld][:channel])
